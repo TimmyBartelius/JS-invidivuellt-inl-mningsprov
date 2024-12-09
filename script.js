efaf40edb83cb030428f2e9aca31d58fbc0834c5
Timmy Bartelius <timmy.bartelius1@gmail.com>
2024-12-09 15:04:27 +0100

    Ändrade kod

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-async function getApiKey(https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/keys){
-    try{
-        const response = await fetch(https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/keys, {
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json',
-            }
-        });
-        if(!response.ok){
-            throw new Error(HTTP error! Status: ${response.status})
-        }
+async function getApiKey() {
+  const url = "https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/keys";
 
-        const data = await response.json();
-        console.log('API key:', data)
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-    catch(error){
-        console.error('Error fetching api key', error)
-    }
+    const data = await response.json();
+    console.log("API key:", data);
+  } catch (error) {
+    console.error("Error fetching api key", error);
+  }
 }
